6214ffccc5069cb9c715e529e8f2557933666944
dsad233 <ksl1421@naver.com>
2024-08-01 07:54:05 +0900

    Test : lint 설정 수정 ci/cd Test

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,37 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import js from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.node },
-  rules : {
-    // 기본적인 룰을 추가해줍니다
-    "constructor-super": "error",
-    "no-const-assign": "error",
-    "no-this-before-super": "error",
-    "no-undef": "error",
-    "no-unreachable": "error",
-    "no-unused-vars": "warn",
-    "no-use-before-define": "error",
+  {
+    "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+    },
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      js,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      "standard", "prettier"
+    ],
+    rules: {
+      'constructor-super': 'error',
+      'no-const-assign': 'error',
+      'no-this-before-super': 'error',
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-unused-vars': 'warn',
+      'no-use-before-define': 'error',
+      "semi": [2, "always"],
+      "no-unused-vars": "warn"
+    },
   },
-},
-  pluginJs.configs.recommended,
 ];
