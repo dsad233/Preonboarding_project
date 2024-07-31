f734ad04d15856a7de6a21dd5cb91aaffac524eb
dsad233 <ksl1421@naver.com>
2024-08-01 07:43:11 +0900

    Test : ci/cd Test2

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,17 @@ import pluginJs from "@eslint/js";
 
 
 export default [
-  {languageOptions: { globals: globals.node }},
+  {languageOptions: { globals: globals.node },
+  rules : {
+    // 기본적인 룰을 추가해줍니다
+    "constructor-super": "error",
+    "no-const-assign": "error",
+    "no-this-before-super": "error",
+    "no-undef": "error",
+    "no-unreachable": "error",
+    "no-unused-vars": "warn",
+    "no-use-before-define": "error",
+  },
+},
   pluginJs.configs.recommended,
 ];
