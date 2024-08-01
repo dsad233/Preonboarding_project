49ff05326a265a4a659739df697a2475f6b6b2ab
dsad233 <ksl1421@naver.com>
2024-08-01 13:51:45 +0900

    Fix : yml 파일 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
 import globals from 'globals';
 import js from '@eslint/js';
+import react from 'eslint-plugin-react';
 
 export default [
   {
     files: ['**/*.js', '**/*.jsx'],
     languageOptions: {
-      globals: globals.node,
+      globals: {
+        ...globals.node,
+        ...globals.es6,
+      },
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
     plugins: {
       js,
+      react,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      "standard", "prettier"
-    ],
     rules: {
       'constructor-super': 'error',
       'no-const-assign': 'error',
@@ -25,8 +25,20 @@ export default [
       'no-unreachable': 'error',
       'no-unused-vars': 'warn',
       'no-use-before-define': 'error',
-      "semi": [2, "always"],
-      "no-unused-vars": "warn"
+      'semi': [2, 'always'],
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.jsx'],
+    extends: ['plugin:react/recommended'],
+  },
+  {
+    files: ['**/*.js'],
+    extends: ['standard', 'prettier'],
   },
 ];
